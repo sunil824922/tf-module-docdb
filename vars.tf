1b4ee20e59ad4bb1a1cc6626979d3fdb2c73ed10
sunil <sunilkumar.ipm@gmail.com>
2023-07-25 19:07:42 +0530

    docdb

--- vars.tf
+++ vars.tf
@@ -1,53 +1,3 @@
-resource "aws_docdb_subnet_group" "main" {
-  name       = "${var.name}-${var.env}"
-  subnet_ids = var.subnets
-
-  tags =  merge(var.tags, {Name = "${var.name}-${var.env}" })
-}
-
-
-resource "aws_security_group" "main" {
-  name        = "${var.name}-${var.env}-sg"
-  description = "${var.name}-${var.env}-sg"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "DOCDB"
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = var.allow_db_cidr
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.name}-${var.env}-sg"
-  }
-}
-
-resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = ""${var.name}-${var.env}""
-  engine                  = "docdb"
-  master_username         = "foo"
-  master_password         = "mustbeeightchars"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-}
-
-
-
-
-
-
-
-
 variable "tags" {}
 variable "env" {}
 variable "subnets" {}
@@ -55,4 +5,9 @@ variable "vpc_id" {}
 variable "allow_db_cidr" {}
 variable "name" {
   default = "docdb"
+}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 27017
 }
